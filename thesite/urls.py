e0b778672b2d38f0230700b363bb65ec46ec7c44
Claire Miller <clairemiller@outlook.com>
2022-06-03 21:24:28 -0700

    Added Login feature and registration, early version

---
+++ thesite/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, DevelopmentView, DevelopmentMapView, TransportMapView, SchoolsMapView, SampleLetterView, ContactView, FireView, EditView
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('developsum/', DevelopmentView.as_view(), name="developsum"),
+    path('developmap/', DevelopmentMapView.as_view(), name="developmap"),
+    path('transportmap/', TransportMapView.as_view(), name="transportmap"),
+    path('schoolsmap/', SchoolsMapView.as_view(), name='schoolsmap'),
+    path('sampleleter/', SampleLetterView.as_view(), name="sampleleter"),
+    path('contact/', ContactView.as_view(), name="contact"),
+    path('fire/', FireView.as_view(), name="fire"),
+    path('editletter/', EditView.as_view(), name="editletter"),
+
+    #path('home/', views.home, name='home'),
+    path('home/', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/edit/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+]
---
+++ thesite/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from .models import Post, Category
+from .forms import PostForm
+from django.urls import reverse_lazy
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+class DevelopmentView(TemplateView):
+    template_name = 'development_sum.html'
+
+class DevelopmentMapView(TemplateView):
+    template_name = 'develop_map.html'
+
+class TransportMapView(TemplateView):
+    template_name = 'transport_map.html'
+
+class SchoolsMapView(TemplateView):
+    template_name = 'schools_map.html'
+
+class SampleLetterView(TemplateView):
+    template_name = 'sample_letter.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+class FireView(TemplateView):
+    template_name = 'fire.html'
+
+class EditView(TemplateView):
+    template_name = 'edit_letter.html'
+
+#def home(request):
+    #return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    #ordering = ['-id']
+    ordering = ['-post_date']
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts':category_posts})
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+    #fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
+    #fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'update_post.html'
+    #fields = ['title', 'author', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
