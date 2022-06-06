0c964b00cc9f33d5459eaa09ee0fe4a57dcb3a20
Claire Miller <clairemiller@outlook.com>
2022-06-05 17:18:34 -0700

    added likes, style updates to forms

--- thesite/models.py
+++ thesite/models.py
@@ -16,13 +16,17 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    header_image = models.ImageField(null =True, blank=True, upload_to="images/")
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     #body = RichTextField(blank=True, null=True)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='uncategorized')
+    likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    def total_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- thesite/urls.py
+++ thesite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, DevelopmentView, DevelopmentMapView, TransportMapView, SchoolsMapView, SampleLetterView, ContactView, FireView, EditView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, DevelopmentView, DevelopmentMapView, TransportMapView, SchoolsMapView, SampleLetterView, ContactView, FireView, EditView, LikeView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -21,4 +21,5 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/edit/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:cats>/', CategoryView, name='category'),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
